03796a0cb2d3a51b0e5385fa40fe7d3c85dd8a24
Jahongir <hamidovjahongir444@gmail.com>
2021-12-08 05:23:53 +0500

    finished

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+app.set('/', 'html')
+app.use(express.static(path.join(__dirname, '/')))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3000, () => console.log('Server running ...'))
